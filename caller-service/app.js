58337cc4d4de31c07337cfb2e0ad6ed6580d2cdc
Canpolat Oral <canpoalotral@gmail.com>
2024-11-13 09:31:15 +0400

    error code feature added

--- caller-service/app.js
+++ caller-service/app.js
@@ -7,18 +7,36 @@ const app = express();
 const serviceName = process.env.SERVICE_NAME || 'caller-service';
 const version = process.env.VERSION || 'v1';
 const targetServiceUrl = process.env.TARGET_SERVICE_URL || 'http://localhost:3000';
+const errorEnabled = process.env.ERROR_ENABLED || false;
+const errorCode = process.env.ERROR_CODE || 200;
+const errorMessage = process.env.ERROR_MESSAGE || 'Error message';
 
-app.get('/', async (req, res) => {
-  try {
-    const targetServiceResponse = await axios.get(targetServiceUrl);
-    res.json({
-      service: serviceName,
-      version: version,
-      accountResponse: targetServiceResponse.data
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Error connecting to account service' });
+
+app.get('/test', async (req, res) => {
+
+  if (errorEnabled) {
+
+    res.status(errorCode).json({ error: errorMessage, service: serviceName, version: version  });
+
+  } else {
+
+    try {
+      const targetServiceResponse = await axios.get(targetServiceUrl + '/test');
+      res.json({
+        service: serviceName,
+        version: version,
+        accountResponse: targetServiceResponse.data
+      });
+    } catch (error) {
+      res.status(200).json({ error: `Error connecting to ${serviceName}` });
+    }
+
   }
+  
+});
+
+app.get('/', (req, res) => {
+  res.json({ service: serviceName, version: version });
 });
 
 const port = process.env.PORT || 3001;
--- receiver-service/app.js
+++ receiver-service/app.js
@@ -5,6 +5,21 @@ const app = express();
 
 const serviceName = process.env.SERVICE_NAME || 'receiver-service';
 const version = process.env.VERSION || 'v1';
+const errorEnabled = process.env.ERROR_ENABLED || false;
+const errorCode = process.env.ERROR_CODE || 200;
+const errorMessage = process.env.ERROR_MESSAGE || 'Error message';
+
+app.get('/test', (req, res) => {
+  if (errorEnabled) {
+
+    res.status(errorCode).json({ error: errorMessage, service: serviceName, version: version  });
+
+  } else {
+
+    res.json({ service: serviceName, version: version });
+
+  }
+});
 
 app.get('/', (req, res) => {
   res.json({ service: serviceName, version: version });
